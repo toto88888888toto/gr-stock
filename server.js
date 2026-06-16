28c2f535fffce91ee4f42f269c9f831aa011f875
Toto <outamavongsa.toto@gmail.com>
2026-06-16 16:56:42 +0700

    Add request logging

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ const upload = multer({
 });
 
 app.use(cors());
+app.use((req, res, next) => { console.log(`[REQ] ${req.method} ${req.path}`); next(); });
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
